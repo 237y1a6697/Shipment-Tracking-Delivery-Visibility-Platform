c030f9a1fc39ca560a57b14a6eced83a3595ad34
prashanth kavuri <prashanthkavuri786@gmail.com>
2026-09-19 13:07:50 +0530

    feat: build authentication foundation

---
+++ backend/src/main/java/com/shiptrack/user/UserRepository.java
@@ -0,0 +1,7 @@
+package com.shiptrack.user;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface UserRepository extends JpaRepository<User,Long> {
+  Optional<User> findByEmail(String email);
+  boolean existsByEmail(String email);
+}
