901937643b080882628b1e31f86bec98ccd3a1e8
prashanth kavuri <prashanthkavuri786@gmail.com>
2026-09-19 13:07:42 +0530

    feat: build authentication foundation

---
+++ backend/src/main/java/com/shiptrack/ShipTrackApplication.java
@@ -0,0 +1,7 @@
+package com.shiptrack;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class ShipTrackApplication {
+  public static void main(String[] args) { SpringApplication.run(ShipTrackApplication.class, args); }
+}
